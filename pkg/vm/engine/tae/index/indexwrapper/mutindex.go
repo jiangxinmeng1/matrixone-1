86376b7680c718d2220028f8fe9e7558fd63ff22
jiangxinmeng1 <jiangxinmeng@matrixorigin.io>
2024-06-04 10:37:26 +0800

    fix check w-w

--- pkg/vm/engine/tae/index/indexwrapper/mutindex.go
+++ pkg/vm/engine/tae/index/indexwrapper/mutindex.go
@@ -168,6 +168,10 @@ func (idx *MutIndex) GetDuplicatedRows(
 		if err == index.ErrNotFound {
 			return nil
 		}
+		err = skipFn(rows[len(rows)-1])
+		if err != nil {
+			return err
+		}
 		var maxRow uint32
 		exist := false
 		for i := len(rows) - 1; i >= 0; i-- {
@@ -179,10 +183,6 @@ func (idx *MutIndex) GetDuplicatedRows(
 		}
 		if !exist {
 			return nil
-		}
-		err = skipFn(maxRow)
-		if err != nil {
-			return err
 		}
 		rowID := objectio.NewRowid(blkID, maxRow)
 		containers.UpdateValue(rowIDs, uint32(offset), *rowID, false, mp)

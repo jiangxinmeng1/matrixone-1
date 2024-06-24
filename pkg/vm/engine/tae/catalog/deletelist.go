c378d4282e73313af6a8a722b400cdb77333e927
XuPeng-SH <xupeng3112@163.com>
2024-06-24 17:03:13 +0800

    update 1

--- pkg/vm/engine/tae/catalog/deletelist.go
+++ pkg/vm/engine/tae/catalog/deletelist.go
@@ -25,42 +25,6 @@ import (
 	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/index"
 )
 
-func (entry *TableEntry) CollectTombstoneInRange(
-	ctx context.Context,
-	start, end types.TS,
-	objectID objectio.ObjectId,
-	mp *mpool.MPool,
-	vpool *containers.VectorPool,
-) (bat *containers.Batch, err error) {
-	it := entry.MakeTombstoneObjectIt(false)
-	for ; it.Valid(); it.Next() {
-		node := it.Get()
-		tombstone := node.GetPayload()
-		tombstone.RLock()
-		skip := tombstone.IsCreatingOrAbortedLocked() || tombstone.HasDropCommittedLocked()
-		tombstone.RUnlock()
-		if skip {
-			continue
-		}
-		visible := tombstone.IsVisibleInRange(start, end)
-		if !visible {
-			continue
-		}
-		if tombstone.HasCommittedPersistedData() {
-			zm := tombstone.GetSortKeyZonemap()
-			if !zm.PrefixEq(objectID[:]) {
-				continue
-			}
-			// TODO: Bloomfilter
-		}
-		err = tombstone.GetObjectData().CollectObjectTombstoneInRange(ctx, start, end, &objectID, &bat, mp, vpool)
-		if err != nil {
-			return nil, err
-		}
-	}
-	return
-}
-
 func (entry *TableEntry) IsDeleted(
 	ctx context.Context,
 	txn txnif.TxnReader,

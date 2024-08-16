e3004be2d8f870f6c05c0bc68a2db68b280ae038
jiangxinmeng1 <jiangxinmeng@matrixorigin.io>
2024-08-16 15:10:10 +0800

    fix ut

--- pkg/vm/engine/tae/db/test/db_test.go
+++ pkg/vm/engine/tae/db/test/db_test.go
@@ -4126,18 +4126,19 @@ func TestBlockRead(t *testing.T) {
 			afterSecondDel := tsAlloc.Alloc()
 
 			tae.CompactBlocks(false)
-			objStats := blkEntry.GetLatestNode().ObjectMVCCNode
-			assert.False(t, objStats.IsEmpty())
-
+			_, rel = tae.GetRelation()
+			tombstoneObjectEntry := testutil.GetOneTombstoneMeta(rel)
+			objectEntry := testutil.GetOneBlockMeta(rel)
+			objStats := tombstoneObjectEntry.ObjectMVCCNode
 			testDS := NewTestBlockReadSource(objStats.ObjectLocation())
 			ds := logtail.NewDeltaLocDataSource(ctx, tae.DB.Runtime.Fs.Service, beforeDel, testDS)
 			bid, _ := blkEntry.ID(), blkEntry.ID()
 
 			info := &objectio.BlockInfo{
 				BlockID:    *objectio.NewBlockidWithObjectID(bid, 0),
-				EntryState: true,
+				EntryState: false,
 			}
-			metaloc := objStats.ObjectLocation()
+			metaloc := objectEntry.ObjectLocation()
 			metaloc.SetRows(schema.BlockMaxRows)
 			info.SetMetaLocation(metaloc)
 

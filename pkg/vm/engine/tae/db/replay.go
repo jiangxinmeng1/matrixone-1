03d082dbc81112161149c9dbfc6f282c2284e5d3
jiangxinmeng1 <jiangxinmeng@matrixorigin.io>
2025-01-03 18:29:27 +0800

    add debug log

--- pkg/vm/engine/tae/db/replay.go
+++ pkg/vm/engine/tae/db/replay.go
@@ -197,7 +197,7 @@ func (replayer *Replayer) OnReplayTxn(cmd txnif.TxnCmd, lsn uint64) {
 		}
 		return
 	}
-	logutil.Infof("lalala lsn %d",lsn)
+	logutil.Infof("lalala lsn %d, ts %v",lsn,txnCmd.PrepareTS.ToString())
 	replayer.applyCount++
 	txn := txnimpl.MakeReplayTxn(replayer.db.Runtime.Options.Ctx, replayer.db.TxnMgr, txnCmd.TxnCtx, lsn,
 		txnCmd, replayer, replayer.db.Catalog, replayer.DataFactory, replayer.db.Wal)
